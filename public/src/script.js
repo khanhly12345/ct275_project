8beb49c17571ebba3d8640322877b79f6799a4f1
khanh ly <lyb2000150@student.ctu.edu.vn>
2023-03-10 22:37:52 +0700

    done js

--- public/src/script.js
+++ public/src/script.js
@@ -1,28 +1,28 @@
 
-    $(".buy_hover").each(function(element){
-        $(this).hover(function(event){
-            $('.buy').each(function(index){
-                if(element==index) {
-                    $('.buy').css('display','block');
-                }
-            })
-        },function(){
-            $('.buy').css('display','none');
-        });
-    })
+    // $(".buy_hover").each(function(element){
+    //     $(this).hover(function(event){
+    //         $('.buy').each(function(index){
+    //             if(element==index) {
+    //                 $('.buy').css('display','block');
+    //             }
+    //         })
+    //     },function(){
+    //         $('.buy').css('display','none');
+    //     });
+    // })
     
-// let buy_hover = document.getElementsByClassName("buy_hover");
-// let buy = document.getElementsByClassName("buy");
-// let img = document.querySelectorAll(".buy_hover img");
-// console.log(buy_hover);
-// console.log(img)
-// for(let i=0; i<buy_hover.length; i++) {
-//     buy_hover[i].addEventListener("mouseover", function() {
-//         buy[i].style.display = "block";
-//         img[i].style.opacity = "0.5";
-//     })
-//     buy_hover[i].addEventListener("mouseout", function() {
-//         buy[i].style.display = "none";
-//         img[i].style.opacity = "1";
-//     })
-// }
+let buy_hover = document.getElementsByClassName("buy_hover");
+let buy = document.getElementsByClassName("buy");
+let img = document.querySelectorAll(".buy_hover img");
+console.log(buy_hover);
+console.log(img)
+for(let i=0; i<buy_hover.length; i++) {
+    buy_hover[i].addEventListener("mouseover", function() {
+        buy[i].style.display = "block";
+        img[i].style.opacity = "0.5";
+    })
+    buy_hover[i].addEventListener("mouseout", function() {
+        buy[i].style.display = "none";
+        img[i].style.opacity = "1";
+    })
+}
